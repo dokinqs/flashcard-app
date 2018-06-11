2c28a9e5fa28bd80d2eeffca5e950b248c66d4a2
Karen Zhao <karen.zhao18@gmail.com>
2018-06-11 11:54:36 -0400

    create

--- flashcard_app/config/application.rb
+++ flashcard_app/config/application.rb
@@ -34,11 +34,15 @@ module FlashcardApp
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.middleware.use Rack::MethodOverride
+    
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
+
+
   end
 end
