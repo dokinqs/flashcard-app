088c91af970937f743ad3caf4e21966d535808fe
Karen Zhao <karen.zhao18@gmail.com>
2019-06-07 17:03:57 -0400

    ud

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Redirect, Link } from 'react-router-dom';
+// import { Redirect, Link } from 'react-router-dom';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
